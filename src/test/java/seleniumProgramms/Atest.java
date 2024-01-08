1200d9105d4fbb42949c062f01d08a279bd695a8
user <user@DESKTOP-EULJV3S>
2024-01-08 17:37:53 +0530

    okjan

---
+++ src/test/java/seleniumProgramms/Atest.java
@@ -0,0 +1,19 @@
+package seleniumProgramms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Atest 
+{
+@Test(groups ="smokeTest")
+public void atest()
+{
+	System.out.println("smoke test case 1");
+ }
+@Test
+public void ates1t()
+{
+	System.out.println("smoke test case no");
+ }
+}
---
+++ src/test/java/seleniumProgramms/Btest.java
@@ -0,0 +1,13 @@
+package seleniumProgramms;
+
+import org.testng.annotations.Test;
+
+public class Btest
+{
+	@Test(groups="smokeTest")
+	public void btes()
+	{
+		System.out.println("smoke test case2");
+	}
+
+}
